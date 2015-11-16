d649e83681ab1b62b1c5709c0e77e337dd44c381
dlunny <dlunny@featurecreeptechnology.com>
2015-11-16 17:09:43 -0500

    Works, in that it passes the test I wrote
    More testing to be done but as of now it appears to be working.

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-var postcss = require('postcss');
+"use strict";
+
+const postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-alphabetize', function (opts) {
     opts = opts || {};
 
     // Work with options here
 
+
+
+
+    //  Returns an array that is alphabatized by a given prop
+    const sortArr = (arr, prop) => {
+      return arr.sort( (a1, a2) => {
+        if( a1[prop] < a2[prop] ) return -1;
+        if( a1[prop] > a2[prop] ) return 1;
+        return 0;
+      });
+    };
+
     return function (css, result) {
-
-        // Transform CSS AST here
-
+      css.walkRules( rule => {
+        //  store our dirty (non-alphabetical) declarations
+        let dirtyDecls = [];
+        rule.nodes.forEach( decl => {
+          dirtyDecls.push({
+            prop: decl.prop,
+            val:  decl.value
+          });
+        })
+        //  set those nodes
+        rule.nodes.forEach( function(decl, index) {
+          rule.nodes[index].prop = cleanDecls[index].prop;
+          rule.nodes[index].value = cleanDecls[index].val;
+        });
+      });
     };
 });
