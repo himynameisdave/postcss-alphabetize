0747c966c98400da2b40afb570cc4ef7a8064458
dlunny <dlunny@featurecreeptechnology.com>
2015-11-16 19:25:09 -0500

    Prefixes stick together, sorta...
    ...they appear after the unprefixed decl so i'd like to change that.

--- index.js
+++ index.js
@@ -17,6 +17,22 @@ module.exports = postcss.plugin('postcss-alphabetize', function (opts) {
         if( a1[prop] > a2[prop] ) return 1;
         return 0;
       });
+    },
+    //  takes in a prop
+    //  returns an object with a boolean (if there is a prefix)
+    //    and it returns what prefix
+    checkPrefixes = ( prop ) => {
+      const prefixes = [ '-webkit-', '-moz-', '-ms-', '-o-' ];
+      let prefix = {
+        isPrefixed: false
+      };
+      prefixes.forEach( pre => {
+        if( prop.indexOf( pre ) > -1 ){
+          prefix.isPrefixed = true;
+          prefix.prefix = pre;
+        }
+      });
+      return prefix;
     };
 
     return function (css, result) {
@@ -24,12 +40,19 @@ module.exports = postcss.plugin('postcss-alphabetize', function (opts) {
         //  store our dirty (non-alphabetical) declarations
         let dirtyDecls = [];
         rule.nodes.forEach( decl => {
-          dirtyDecls.push({
+          let dirtyDeclare = {
             prop: decl.prop,
-            val:  decl.value
-          });
+            val:  decl.value,
+            prefixed: checkPrefixes(decl.prop)
+          };
+          if(dirtyDeclare.prefixed.isPrefixed){
+            dirtyDeclare.sortName = decl.prop.split(dirtyDeclare.prefixed.prefix)[1];
+          }else{
+            dirtyDeclare.sortName = decl.prop;
+          }
+          dirtyDecls.push(dirtyDeclare);
         })
-        let cleanDecls = sortArr( dirtyDecls, 'prop' );
+        let cleanDecls = sortArr( dirtyDecls, 'sortName' );
         //  set those nodes?
         rule.nodes.forEach( function(decl, index) {
           rule.nodes[index].prop = cleanDecls[index].prop;
